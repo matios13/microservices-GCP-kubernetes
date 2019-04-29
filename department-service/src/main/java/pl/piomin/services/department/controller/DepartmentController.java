6ecb0bbf2bf9eeefca3ae2f39b3404a38a4af58c
Mateusz.Manka <mateusz.manka@eclipsegroup.co.uk>
2019-04-29 13:10:30 +0200

    Change request mapping to work with ingress

--- department-service/src/main/java/pl/piomin/services/department/controller/DepartmentController.java
+++ department-service/src/main/java/pl/piomin/services/department/controller/DepartmentController.java
@@ -5,17 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import pl.piomin.services.department.client.EmployeeClient;
 import pl.piomin.services.department.model.Department;
 import pl.piomin.services.department.repository.DepartmentRepository;
 
 @RestController
+@RequestMapping("/department")
 public class DepartmentController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
